16126b7a56b1ab8e38bb1cb6fdccdea141288317
semssk <ssksem015@gmail.com>
2023-03-25 14:46:15 +0100

    Added doc to auth module

--- src/apis/mod.rs
+++ src/apis/mod.rs
@@ -1,1 +1,7 @@
+pub mod admin;
+
+/// this module contains Authentication related functions which are
+/// Http Post Login
+/// Http Get  Refresh
+/// Security wrapping function
 pub mod authentication;
