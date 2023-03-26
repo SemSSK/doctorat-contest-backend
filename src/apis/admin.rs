261f2c27c5fa37b4a6994a6ccb879193a2ab4f9a
semssk <ssksem015@gmail.com>
2023-03-26 14:44:54 +0100

    reorganization of the admin module separating database access into sub module exposing only the rest apis

--- src/apis/admin.rs
+++ src/apis/admin.rs
@@ -2,15 +2,17 @@ use crate::apis::authentication::secure_function;
 use crate::model::user;
 use crate::ServerState;
 use actix_web::{delete, get, post, put, web, Either, HttpRequest, HttpResponse, Responder};
-use rand::distributions::Alphanumeric;
-use rand::Rng;
 use serde::{Deserialize, Serialize};
 
-/// Gets one user from database by id
-async fn get_user_from_db(id: i32, pool: &sqlx::MySqlPool) -> sqlx::Result<user::User> {
-    sqlx::query_as!(
-        user::User,
-        r#"
+mod db {
+    use crate::model::user;
+    use rand::{distributions::Alphanumeric, Rng};
+
+    /// Gets one user from database by id
+    pub async fn get_user(id: i32, pool: &sqlx::MySqlPool) -> sqlx::Result<user::User> {
+        sqlx::query_as!(
+            user::User,
+            r#"
           select 
             id as 'id?', 
             email, password as 'password?', 
@@ -20,10 +22,114 @@ async fn get_user_from_db(id: i32, pool: &sqlx::MySqlPool) -> sqlx::Result<user:
           from Edl.User
           where id = ?
         "#,
-        id
-    )
-    .fetch_one(pool)
-    .await
+            id
+        )
+        .fetch_one(pool)
+        .await
+    }
+
+    /// Get all users from database
+    pub async fn get_users(pool: &sqlx::MySqlPool) -> sqlx::Result<Vec<user::User>> {
+        sqlx::query_as!(
+            user::User,
+            r#"
+        select 
+          id as 'id?', 
+          email, password as 'password?', 
+          role as 'role?: user::Role',
+          encoded as 'encoded?',
+          specialty as 'specialty?'
+        from Edl.User
+      "#
+        )
+        .fetch_all(pool)
+        .await
+    }
+
+    pub async fn get_user_by_email(
+        email: &str,
+        pool: &sqlx::MySqlPool,
+    ) -> sqlx::Result<user::User> {
+        sqlx::query_as!(
+            user::User,
+            r#"
+            select 
+              id as 'id?', 
+              email, password as 'password?', 
+              role as 'role?: user::Role',
+              encoded as 'encoded?',
+              specialty as 'specialty?'
+              from Edl.User
+            where email = ?
+          "#,
+            email
+        )
+        .fetch_one(pool)
+        .await
+    }
+
+    fn generate_password() -> String {
+        let s: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+        // let s = bcrypt::hash(s, DEFAULT_COST).unwrap();
+        s
+    }
+
+    pub async fn insert_user(
+        email: String,
+        role: user::Role,
+        specialty: String,
+        pool: &sqlx::MySqlPool,
+    ) -> sqlx::Result<()> {
+        let password = generate_password();
+        sqlx::query!(
+            r#"
+          insert into Edl.User (email,password,role,specialty) values
+          (?,?,?,?)
+        "#,
+            email,
+            password,
+            role,
+            specialty
+        )
+        .execute(pool)
+        .await?;
+        Ok(())
+    }
+
+    /// updates one user given a user object
+    pub async fn update_user(u: user::User, pool: &sqlx::MySqlPool) -> sqlx::Result<()> {
+        sqlx::query!(
+            r#"
+    update Edl.User
+    set email = ?, role = ?, specialty = ?
+    where id = ?
+  "#,
+            u.email,
+            u.role,
+            u.specialty,
+            u.id
+        )
+        .execute(pool)
+        .await?;
+        Ok(())
+    }
+
+    pub async fn delete_user(id: i32, pool: &sqlx::MySqlPool) -> sqlx::Result<()> {
+        sqlx::query!(
+            r#"
+    delete from Edl.User
+    where id = ?
+  "#,
+            id
+        )
+        .execute(pool)
+        .await?;
+        Ok(())
+    }
 }
 
 #[get("/{id}")]
@@ -35,7 +141,7 @@ pub async fn get_user(
     let id = path.0;
     let Some(f) = secure_function(
         |_| true,
-        |_| get_user_from_db(id, &data.pool),
+        |_| db::get_user(id, &data.pool),
         &[user::Role::Admin],
         request,
     ) else {
@@ -47,30 +153,12 @@ pub async fn get_user(
     Either::Right(web::Json(u))
 }
 
-/// Get all users from database
-async fn get_users_from_db(pool: &sqlx::MySqlPool) -> sqlx::Result<Vec<user::User>> {
-    sqlx::query_as!(
-        user::User,
-        r#"
-      select 
-        id as 'id?', 
-        email, password as 'password?', 
-        role as 'role?: user::Role',
-        encoded as 'encoded?',
-        specialty as 'specialty?'
-      from Edl.User
-    "#
-    )
-    .fetch_all(pool)
-    .await
-}
-
 #[get("/")]
 async fn get_users(
     data: web::Data<ServerState>,
     request: HttpRequest,
 ) -> Either<HttpResponse, impl Responder> {
-    let Some(f) = secure_function(|_| true, |_| get_users_from_db(&data.pool),&[user::Role::Admin], request) else {
+    let Some(f) = secure_function(|_| true, |_| db::get_users(&data.pool),&[user::Role::Admin], request) else {
       return Either::Left(HttpResponse::Forbidden().finish());
     };
 
@@ -79,57 +167,6 @@ async fn get_users(
     };
 
     Either::Right(web::Json(users))
-}
-
-async fn get_user_by_email(email: &str, pool: &sqlx::MySqlPool) -> sqlx::Result<user::User> {
-    sqlx::query_as!(
-        user::User,
-        r#"
-          select 
-            id as 'id?', 
-            email, password as 'password?', 
-            role as 'role?: user::Role',
-            encoded as 'encoded?',
-            specialty as 'specialty?'
-            from Edl.User
-          where email = ?
-        "#,
-        email
-    )
-    .fetch_one(pool)
-    .await
-}
-
-fn generate_password() -> String {
-    let s: String = rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(7)
-        .map(char::from)
-        .collect();
-    // let s = bcrypt::hash(s, DEFAULT_COST).unwrap();
-    s
-}
-
-async fn insert_user(
-    email: String,
-    role: user::Role,
-    specialty: String,
-    pool: &sqlx::MySqlPool,
-) -> sqlx::Result<()> {
-    let password = generate_password();
-    sqlx::query!(
-        r#"
-          insert into Edl.User (email,password,role,specialty) values
-          (?,?,?,?)
-        "#,
-        email,
-        password,
-        role,
-        specialty
-    )
-    .execute(pool)
-    .await?;
-    Ok(())
 }
 
 #[derive(Serialize, Deserialize)]
@@ -153,7 +190,7 @@ async fn create_user(
 
     let Some(f) = secure_function(
         |_| true,
-        |_| insert_user(email, role, specialty, &data.pool),
+        |_| db::insert_user(email, role, specialty, &data.pool),
         &[user::Role::Admin],
         request,
     ) else {
@@ -165,23 +202,6 @@ async fn create_user(
     };
 
     Either::Right(HttpResponse::Ok().finish())
-}
-
-async fn update_user_in_db(u: user::User, pool: &sqlx::MySqlPool) -> sqlx::Result<()> {
-    sqlx::query!(
-        r#"
-      update Edl.User
-      set email = ?, role = ?, specialty = ?
-      where id = ?
-    "#,
-        u.email,
-        u.role,
-        u.specialty,
-        u.id
-    )
-    .execute(pool)
-    .await?;
-    Ok(())
 }
 
 #[put("/")]
@@ -192,7 +212,7 @@ async fn update_user(
 ) -> Either<HttpResponse, impl Responder> {
     let Some(f) = secure_function(
         |_| true,
-        |_| update_user_in_db(u.0, &data.pool),
+        |_| db::update_user(u.0, &data.pool),
         &[user::Role::Admin],
         request,
     ) else {
@@ -204,19 +224,6 @@ async fn update_user(
     };
 
     Either::Right(HttpResponse::Ok().finish())
-}
-
-async fn delete_user_from_db(id: i32, pool: &sqlx::MySqlPool) -> sqlx::Result<()> {
-    sqlx::query!(
-        r#"
-      delete from Edl.User
-      where id = ?
-    "#,
-        id
-    )
-    .execute(pool)
-    .await?;
-    Ok(())
 }
 
 #[delete("/{id}")]
@@ -228,7 +235,7 @@ pub async fn delete_user(
     let id = path.0;
     let Some(f) = secure_function(
         |_| true,
-        |_| delete_user_from_db(id, &data.pool),
+        |_| db::delete_user(id, &data.pool),
         &[user::Role::Admin],
         request,
     ) else {
